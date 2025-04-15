05b1d71bc92317c102d724374c68bd79ee0e144b
TazoGit <vadachkoriatazo@gmail.com>
2025-04-15 23:49:51 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 
+    const planetNameEl = document.getElementById('planet-name');
+    const planetImageEl = document.getElementById('planet-image');
+    const planetDescEl = document.getElementById('planet-description');
+    const planetSourceEl = document.getElementById('planet-source');
+    const rotationTimeEl = document.getElementById('rotation-time');
+    const revolutionTimeEl = document.getElementById('revolution-time');
+    const radiusEl = document.getElementById('radius');
+    const temperatureEl = document.getElementById('temperature');
+    const planetLinks = document.querySelectorAll('.planet-nav a');
+    const tabs = document.querySelectorAll('.tab');
+
 
 
 
